0f5c7df957387fa1f39266192661e5468275d51a
juliu <juliu@192.168.24.221>
2021-12-19 20:01:12 +0100

    JUnit Test Navigation

--- src/test/java/JUnitNavigation.java
+++ src/test/java/JUnitNavigation.java
@@ -2,11 +2,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Multimedia.Navigation;
+
 class JUnitNavigation {
 
 	@Test
 	void test() {
 		fail("Not yet implemented");
+	}
+	@Test
+	public void navigationTest() {
+		assertEquals("Berlin", Navigation.Stadtnamen("Berlin"));
 	}
 
 }
