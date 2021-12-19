a82a2cb3c5c9562c638b633d0f462a2fbe0f1c79
juliu <juliu@192.168.24.221>
2021-12-19 19:24:27 +0100

    Erweiterung Methode Navigation (Julius)

--- src/main/java/Multimedia/Navigation.java
+++ src/main/java/Multimedia/Navigation.java
@@ -11,7 +11,30 @@ public class Navigation {
 			if (stadt == stadtnamen[i]) 
 				System.out.println(stadtnamen[i]);	
 		}
-
+		
+		 switch(stadt){
+	        case "Hamburg":
+	            System.out.println("Postleitzah: 20095 ");
+	            break;
+	        case "Stuttgart":
+	        	System.out.println("Postleitzah: 70173 ");
+	            break;
+	        case "Berlin":
+	        	System.out.println("Postleitzah: 10115 ");
+	            break;
+	        case "Dortmund":
+	        	System.out.println("Postleitzah: 44263");
+	            break;
+	        case "Paris":
+	        	System.out.println("Postleitzah: 75116");
+	            break;
+	        case "Rom":
+	        	System.out.println("Postleitzah: 00017");
+	            break;
+	        default:
+	            System.out.println("Die Eingabe war nicht korrekt");
+	            break;
+		 }
 		return stadt;
 	}
 }
