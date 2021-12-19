b955ba4f9bf37ff397d216b2db1a1b3c1837490b
juliu <juliu@192.168.24.221>
2021-12-19 19:07:43 +0100

    Methode Navigation

--- src/main/java/Multimedia/Navigation.java
+++ src/main/java/Multimedia/Navigation.java
@@ -3,15 +3,15 @@ package Multimedia;
 import java.util.ArrayList;
 
 public class Navigation {
-	
-	
-	
-	public static ArrayList<String> Stadtnamen() {
-		String[] stadtnamen = {"Stuttgart","Hamburg", "Berlin","Dortmund","Paris","Rom"};
-		
-		return null;
+
+	public static String Stadtnamen(String stadt) {
+		String[] stadtnamen = { "Stuttgart", "Hamburg", "Berlin", "Dortmund", "Paris", "Rom" };
+
+		for (int i = 0; i < stadtnamen.length; i++) {
+			if (stadt == stadtnamen[i]) 
+				System.out.println(stadtnamen[i]);	
+		}
+
+		return stadt;
 	}
-	
-	
-	
 }
