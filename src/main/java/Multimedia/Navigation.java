7ea277c36909dbc95501f2834f64ef03e75ae427
Victor Cölsch <72910333+vcoelsch@users.noreply.github.com>
2021-12-19 22:35:19 +0100

    Funktionsweise angepasst für sinnvollen Test

--- src/main/java/Multimedia/Navigation.java
+++ src/main/java/Multimedia/Navigation.java
@@ -3,37 +3,29 @@ package Multimedia;
 
 public class Navigation {
 
-	public static String Stadtnamen(String stadt) {
-		String[] stadtnamen = { "Stuttgart", "Hamburg", "Berlin", "Dortmund", "Paris", "Rom" };
+    public static String Stadtnamen(String stadt) {
+        String[] stadtnamen = { "Stuttgart", "Hamburg", "Berlin", "Dortmund", "Paris", "Rom" };
 
-		for (int i = 0; i < stadtnamen.length; i++) {
-			if (stadt == stadtnamen[i]) 
-				System.out.println(stadtnamen[i]);	
-		}
-		
-		 switch(stadt){
-	        case "Hamburg":
-	            System.out.println("Postleitzah: 20095 ");
-	            break;
-	        case "Stuttgart":
-	        	System.out.println("Postleitzah: 70173 ");
-	            break;
-	        case "Berlin":
-	        	System.out.println("Postleitzah: 10115 ");
-	            break;
-	        case "Dortmund":
-	        	System.out.println("Postleitzah: 44263");
-	            break;
-	        case "Paris":
-	        	System.out.println("Postleitzah: 75116");
-	            break;
-	        case "Rom":
-	        	System.out.println("Postleitzah: 00017");
-	            break;
-	        default:
-	            System.out.println("Die Eingabe war nicht korrekt");
-	            break;
-		 }
-		return stadt;
-	}
+        for (int i = 0; i < stadtnamen.length; i++) {
+            if (stadt == stadtnamen[i])
+                switch (stadt) {
+                case "Hamburg":
+                    return stadt + " Postleitzah: 20095";
+                case "Stuttgart":
+                    return stadt + " Postleitzah: 70173";
+                case "Berlin":
+                    return stadt + " Postleitzah: 10115";
+                case "Dortmund":
+                    return stadt + " Postleitzah: 44263";
+                case "Paris":
+                    return stadt + " Postleitzah: 75116";
+                case "Rom":
+                    return stadt + " Postleitzah: 00017";
+                default:
+                    break;
+                }
+        }
+
+        return "Wir können leider keine Adresse ausmachen.";
+    }
 }
