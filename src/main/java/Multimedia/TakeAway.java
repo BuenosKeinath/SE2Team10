da6e099be34f2fc7178ca48e08f2102341763388
Moritz44 <95297970+Moritz44@users.noreply.github.com>
2021-12-19 19:54:03 +0100

    Add files via upload
    [Take-Away]
    Mit dieser Funktion kann man das Tablet vom Fahrzeug entkoppeln oder wieder verbinden.

---
+++ src/main/java/Multimedia/TakeAway.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TakeAway {
+
+	public static void takeAway() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Wilkommen bei der Take-Away Funktion, um ihr Tablet zu entkoppeln schalten sie bitte das Fahrzeug aus [Enter zum fortfahren]");
+		input.nextLine();
+
+		System.out.println("Möchten sie das Tablet jetzt entkoppen?" + "\n" + "Enter [1] für Bestätigen oder [2] für Abbruch ");
+		String entkoppeln = input.nextLine();
+
+		if (entkoppeln.contains("1")) {
+			System.out.println("Ihr Gerät wurde erfolgreich entkoppelt. [Enter zum fortfahren]");
+			input.nextLine();
+
+			System.out.println("Möchten sie ihr Tablet wieder mit dem Fahrzeug verbinden? Enter [1] für Bestätigen oder [2] für Abbruch");
+			String verbinden = input.nextLine();
+
+			if (verbinden.contains("1")) {
+				System.out.println("Ihr Gerät wurde erfolgreich mit dem Fahrzeug verbunden.");
+			} else if (verbinden.contains("2")) {
+				System.out.println("Der Verbindungsvorgrang wurde abgebrochen.");
+			}
+
+		} else if (entkoppeln.contains("2")) {
+			System.out.println("Der Entkopplungsvorgang wurde abgebrochen.");
+		}
+
+	}
+
+}
